45039b4268332f1299aa2551e7704ab10ca92266
CiscG <fgss98@gmail.com>
2026-01-12 11:56:00 -0300

    ipdate springboot

--- SpringBoot/api/src/main/java/br/com/projetoaula/api/controle/Controle.java
+++ SpringBoot/api/src/main/java/br/com/projetoaula/api/controle/Controle.java
@@ -1,6 +1,10 @@
 package br.com.projetoaula.api.controle;
 
+import br.com.projetoaula.api.modelo.Pessoa;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,8 +17,18 @@ public class Controle {
     }
 
     @GetMapping("/boasvindas")
-    public String BoasVindas(){
+    public String boasVindas(){
         return "Seja bem vindo(a)";
+    }
+
+    @GetMapping("/boasvindas/{nome}")
+    public String boasVindas(@PathVariable String nome){
+        return "Seja bem vindo(a) " + nome;
+    }
+
+    @PostMapping("/pessoa")
+    public Pessoa pessoa(@RequestBody Pessoa p){
+        return p;
     }
 
 }
